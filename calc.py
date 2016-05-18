33fb53974c7f436bd40d1433ed1d3f85dda8a9bb
Devangi Vivrekar <devangi@stanford.edu>
2016-05-17 18:34:28 -0700

    Lab 13 - calculator works but haven't gotten to testing yet

--- calc.py
+++ calc.py
@@ -5,28 +5,50 @@
 # Spring 2016
 
 import sys
+import re
 
 def main():
     """Join command-line arguments and pass them to unitcalc(), then print."""
     calculation = ''.join(sys.argv[1:])
     print calc(calculation)
-
+    
+#given two numbers and a calculation, returns the appropriate output    
+def intermediate_result(num1, num2, operation):
+        if operation=='+':
+            return float(num1)+float(num2)
+        elif operation=='-':
+            return float(num1)-float(num2)
+        elif operation=='*':
+            return float(num1)*float(num2)
+        elif operation=='/':
+            return float(num1)/float(num2)    
 def calc(s):
     """Parse a string describing an operation on quantities with units."""
+    
+    nums = []
+    operations = []
+    curr_index = 0
+    while curr_index < len(s):
+        if (not s[curr_index].isdigit()):
+            nums.append(float(s[0:curr_index]))
+            operations.append(s[curr_index])
+            s = s[curr_index+1:len(s)]
+            curr_index = 0
+        curr_index += 1
+    nums.append(float(s))        
 
-    # TODO make this robust for differently formatted inputs
-    num1 = s[0]
-    num2 = s[2]
-    operation = s[1]
-
-    if operation=='+':
-        return int(num1)+int(num2)
-    elif operation=='-':
-        pass
-    elif operation=='*':
-        pass
-    elif operation=='/':
-        pass
-
-
+    #at this point the lists nums and operations are populated with all the numbers and operations entered
+    
+    #checks for syntax
+    assert(len(nums) >= 2)
+    assert(len(operations) >= 1)
+        
+    #This does not take into account order of operations but instead just does the computations in the order they are written
+    result = intermediate_result(nums[0], nums[1], operations[0])
+    for i in range(2, len(nums)):
+        result = intermediate_result(result, nums[i], operations[i-1])
+        
+    print(result)
+     
+        
 if __name__ == "__main__": main()
